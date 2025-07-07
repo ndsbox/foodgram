e99b915c92cfc45115b457f2281415b71cafc463
ndsbox <ndsbox@mail.ru>
2025-07-07 11:46:17 +0300

    start_ndsboxfoodgram_on_9100

---
+++ backend/foodgram/constants.py
@@ -0,0 +1,8 @@
+from django.core.validators import RegexValidator
+
+REGEX_COLOR = RegexValidator(r'^#[0-9A-F]{6}$', 'Поддерживаемые знаки.')
+
+NAME_MAX_LENGTH = 150
+TEXT_MAX_LENGTH = 200
+
+DEFAULT_PAGES_LIMIT = 6
