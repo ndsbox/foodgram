20e805341e1949743ffd8aa365b8ef3db0443e4d
ndsbox <ndsbox@mail.ru>
2025-07-08 13:56:23 +0300

    env enabled

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,8 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
