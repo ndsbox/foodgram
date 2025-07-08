ce3f021971df05daa851594c466e929dcfe20ba3
ndsbox <ndsbox@mail.ru>
2025-07-08 15:09:13 +0300

    one more try error500

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
