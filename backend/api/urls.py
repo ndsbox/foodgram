48c5c741ea8ee7e5ae05e3582bb5e8365f2c584a
ndsbox <ndsbox@mail.ru>
2025-07-15 13:43:09 +0300

    flake8 issue

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,7 +15,9 @@ app_name = 'api'
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/login/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('auth/', include('djoser.urls.authtoken')),
 ]
